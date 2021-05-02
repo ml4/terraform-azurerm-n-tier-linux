2ca9177092da6ce553ae65e415eb8e72209da926
ml4 <ml4@psy.industries>
2021-05-02 23:36:50 +0100

    Add initial files

--- main.tf
+++ main.tf
@@ -1,2 +1,38 @@
 //// main.tf terraform configuration
 //
+resource "azurerm_network_interface" "main" {
+  name                = "${azurerm_linux_virtual_machine.name}-nic"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "${var.prefix}-${var.instance_config.vm_name}"
+  resource_group_name = var.rg_name
+  location            = azurerm_resource_group.main.location
+  size                = var.instance_config.machine_size
+  admin_username      = var.instance_config.admin_username
+  admin_password      = var.instance_config.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = var.instance_config.os_disk_storage_account_type
+  }
+
+  // source_image_id can replace this if business packer base images are to be used
+  source_image_reference {
+    publisher = var.instance_config.os_publisher
+    offer     = var.instance_config.os_offer
+    sku       = var.instance_config.os_sku
+    version   = var.instance_config.os_version
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,5 +1,45 @@
-// variable "prefix" {
-//   type        = string
-//   description = "Currently, the prefix equals the whole key name"
-// }
+variable "prefix" {
+  type        = string
+  description = "Main prefix for all objects"
+}
 
+variable "rg_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Location to deploy to"
+  default     = "West Europe"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "ID of a single vnet subnet"
+}
+
+variable "instance_config" {
+  type = object({
+    vm_name                      = string
+    os_publisher                 = string
+    os_offer                     = string
+    os_sku                       = string
+    os_version                   = string
+    os_disk_storage_account_type = string
+    machine_size                 = string
+    admin_username               = string
+    admin_password               = string
+  })
+  default = {
+    vm_name                      = "azure-linuxvm"
+    os_publisher                 = "MicrosoftWindowsServer"
+    os_offer                     = "WindowsServer"
+    os_sku                       = "2016-Datacenter"
+    os_version                   = "latest"
+    os_disk_storage_account_type = "Standard_LRS"
+    machine_size                 = "Standard_F2"
+    admin_username               = "adminuser"
+    admin_password               = ""
+  }
+}
