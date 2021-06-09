d9ee9fa8958939231537c71b475fd22488d06943
ml4 <ml4@psy.industries>
2021-06-09 22:35:02 +0100

    Bump mods, fix pwd, format

--- examples/main.tf
+++ examples/main.tf
@@ -6,15 +6,15 @@
 //
 module "n-tier-linux-web" {
   source    = "app.terraform.io/ml4-hc/n-tier-linux/azurerm"
-  version   = "1.0.22"
+  version   = "1.0.23"
   prefix    = "ml4"
-  rg_name   = "test_net_rg-web"
+  rg_name   = "test_net_rg_linux_web"
   location  = "West Europe"
   subnet_id = module.network-vnet.avn-as-public-subnet_id[0]
 
   web = true
   web_instance_config = {
-    vm_name                      = "rmg-test-web"
+    vm_name                      = "test-web"
     machine_size                 = "Standard_D2_v4"
     os_publisher                 = "RedHat"
     os_offer                     = "RHEL"
@@ -22,21 +22,21 @@ module "n-tier-linux-web" {
     os_version                   = "7.9.2021051701"
     os_disk_storage_account_type = "Standard_LRS"
     admin_username               = "rhel"
-    admin_password               = "KHvYv^mMqwQh" // obviously don't commit a real password and instead, get it from a secrets manager
+    admin_password               = "donotusethispasswordN123_" // obviously don't commit a real password and instead, get it from a secrets manager
   }
 }
 
 module "n-tier-linux-app" {
   source    = "app.terraform.io/ml4-hc/n-tier-linux/azurerm"
-  version   = "1.0.22"
+  version   = "1.0.23"
   prefix    = "ml4"
-  rg_name   = "test_net_rg-app"
+  rg_name   = "test_net_rg_linux_app"
   location  = "West Europe"
   subnet_id = module.network-vnet.avn-as-public-subnet_id[0]
 
   app = true
   app_instance_config = {
-    vm_name                      = "rmg-test-app"
+    vm_name                      = "test-app"
     machine_size                 = "Standard_D2_v4"
     os_publisher                 = "RedHat"
     os_offer                     = "RHEL"
@@ -44,21 +44,21 @@ module "n-tier-linux-app" {
     os_version                   = "7.9.2021051701"
     os_disk_storage_account_type = "Standard_LRS"
     admin_username               = "rhel"
-    admin_password               = "KHvYv^mMqwQh" // obviously don't commit a real password and instead, get it from a secrets manager
+    admin_password               = "donotusethispasswordN123_" // obviously don't commit a real password and instead, get it from a secrets manager
   }
 }
 
 module "n-tier-linux-db" {
   source    = "app.terraform.io/ml4-hc/n-tier-linux/azurerm"
-  version   = "1.0.22"
+  version   = "1.0.23"
   prefix    = "ml4"
-  rg_name   = "test_net_rg-db"
+  rg_name   = "test_net_rg_linux_db"
   location  = "West Europe"
   subnet_id = module.network-vnet.avn-as-public-subnet_id[0]
 
   database = true
   db_instance_config = {
-    vm_name                      = "rmg-test-db"
+    vm_name                      = "test-db"
     machine_size                 = "Standard_D2_v4"
     os_publisher                 = "RedHat"
     os_offer                     = "RHEL"
@@ -66,7 +66,7 @@ module "n-tier-linux-db" {
     os_version                   = "7.9.2021051701"
     os_disk_storage_account_type = "Standard_LRS"
     admin_username               = "rhel"
-    admin_password               = "KHvYv^mMqwQh" // obviously don't commit a real password and instead, get it from a secrets manager
+    admin_password               = "donotusethispasswordN123_" // obviously don't commit a real password and instead, get it from a secrets manager
   }
 }
 
